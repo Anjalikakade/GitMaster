b8ab8a962ccfc2d0834d8da8a99990bbb90315e7
Anjali <anjalik2997@gmail.com>
2024-08-01 15:37:05 +0530

    Original code

---
+++ src/AllBasic/Hello.java
@@ -0,0 +1,12 @@
+package AllBasic;
+
+public class Hello {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hiiiiiiiii");
+		System.out.println("Anjali kakade Master");
+
+	}
+
+}
